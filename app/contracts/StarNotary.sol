2e44bd104cffa99b1183c6af6b30f019cfc004e8
mrahman <rahmanww@gmail.com>
2019-07-09 15:25:02 -0400

    Dapp on Ethereum

--- app/contracts/StarNotary.sol
+++ app/contracts/StarNotary.sol
@@ -1,88 +1,110 @@
-pragma solidity >=0.4.10 <0.6.0;
+//pragma solidity >=0.4.10 <0.6.0;
+pragma solidity 0.5.0;
 
-import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+//Importing openzeppelin-solidity ERC-721 implemented Standard
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
-// StarNotary Contract declaration inheritance the ERC721 openzeppelin implementation
-contract StarNotary is ERC721 {
 
-    event starRegistered(address registrant);
+/** StarNotary Contract declaration inheritance the ERC721 openzeppelin implementation */
+contract StarNotary is ERC721Full, ERC721Mintable {
 
-    struct Star { 
+    // Star data
+    struct Star {
         string name;
-        string starStory;
-        string ra;
-        string dec;
-        string mag;
-        // bool isRegistered;
     }
 
-    mapping(uint256 => Star) public tokenIdToStarInfo; 
+    /**
+       Initialise name and symbol properties through the constructor
+       as per example on https://www.npmjs.com/package/openzeppelin-solidity
+     */
+    constructor() ERC721Full("StarNotary Token", "SUN") public {
+    }
+
+    // mapping the Star with the Owner Address
+    mapping(uint256 => Star) public tokenIdToStarInfo;
+
+    // mapping the TokenId and price
     mapping(uint256 => uint256) public starsForSale;
-    // below mapping must be private or cause error: 'Accessors for mapping with dynamically-sized keys not yet implemented.'
-    mapping(string => bool) private starRegistry;
 
-
-    function createStar(string memory _name, string memory _starStory, string memory _ra, string memory _dec, string memory _mag, uint256  _tokenId) public { 
-        
-        // check if this star is available to register
-        // note: if it does not exist, it will return 0, which is equal to 'false'
-        require(!this.isStarRegistered(_ra, _dec, _mag), "This star is already registerd!");
-
-        // create a new star object; for now store it in memory
-        Star memory newStar = Star(_name, _starStory, _ra, _dec, _mag);
-
-        // register the new star information as a token
+    // Create Star using the Struct
+    function createStar(string memory _name, uint256 _tokenId) public {
+        // Passing the name and tokenId as a parameters
+        Star memory newStar = Star(_name);
+        // Star is an struct so we are creating a new Star
         tokenIdToStarInfo[_tokenId] = newStar;
-        // register the new star into the starRegistery to prevent duplicated register request
-        starRegistry[this.append(_ra, _dec, _mag)] = true;
-
-        // mint: making a new special token. Remember each star is a unique ERC721 token
-        // https://openzeppelin.org/api/docs/1.6.0/token_ERC721_ERC721Token.html#_mint
+        // Creating in memory the Star -> tokenId mapping
         _mint(msg.sender, _tokenId);
-
-        emit starRegistered(msg.sender);
+        // _mint assign the the star with _tokenId to the sender address (ownership)
     }
 
-    function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
-        require(this.ownerOf(_tokenId) == msg.sender, "Only the owner of this star has the right to sale it.");
-
+    /**
+       Putting an Star for sale (Adding the star tokenid into the mapping starsForSale,
+       first verify that the sender is the owner)
+      */
+    function putStarUpForSale(uint256 _tokenId, uint256 _price) public {
+        require(ownerOf(_tokenId) == msg.sender, "You can't sell a Star you don't own");
         starsForSale[_tokenId] = _price;
     }
 
-    function buyStar(uint256 _tokenId) public payable { 
-        require(starsForSale[_tokenId] > 0, "Not a valid start to buy.");
-
+    function buyStar(uint256 _tokenId) public payable {
+        require(starsForSale[_tokenId] > 0, "The Star should be up for sale");
         uint256 starCost = starsForSale[_tokenId];
-        address starOwner = this.ownerOf(_tokenId);
-        
-        require(msg.value >= starCost, "You don't have enough ETH to purchase this star.");
-
-        _removeTokenFrom(starOwner, _tokenId);
-        _addTokenTo(msg.sender, _tokenId);
-
-        starOwner.transfer(starCost);
-
-        if(msg.value > starCost) { 
+        address ownerAddress = ownerOf(_tokenId);
+        require(msg.value > starCost, "You need to have enough Ether");
+        _transferFrom(ownerAddress, msg.sender, _tokenId);
+        // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
+        address payable ownerAddressPayable = _makePayable(ownerAddress);
+        // We need to make this conversion to be able to use transfer() function to transfer ethers
+        ownerAddressPayable.transfer(starCost);
+        if (msg.value > starCost) {
             msg.sender.transfer(msg.value - starCost);
         }
     }
 
-    function tokenIDToStarInfo(uint256 _tokenId) public view returns (string memory name, string memory starStory, string memory ra, string memory dec, string memory mag) {
-        return (tokenIdToStarInfo[_tokenId].name,
-            tokenIdToStarInfo[_tokenId].starStory,
-            tokenIdToStarInfo[_tokenId].ra,
-            tokenIdToStarInfo[_tokenId].dec,
-            tokenIdToStarInfo[_tokenId].mag);
+    /**
+      Get the Star (struct) and return its name property
+      */
+    function lookUptokenIdToStarInfo(uint _tokenId) public view returns (string memory) {
+        return tokenIdToStarInfo[_tokenId].name;
     }
 
-    // method borrowed from https://ethereum.stackexchange.com/questions/729/how-to-concatenate-strings-in-solidity
-    // Using ABI encoding functions to append star coordinates into one long string
-    function append(string memory _ra, string memory _dec, string memory _mag) public pure returns (string memory) {
+    /**
+      * Exchanges two stars if the sender owns one of them
+      */
+    function exchangeStars(uint256 _tokenId1, uint256 _tokenId2) public {
+        // Verify that the sender owns one of the tokens
+        if (msg.sender != ownerOf(_tokenId1) && msg.sender != ownerOf(_tokenId2)) {
+            revert("You need to be the owner of one of the stars to exchange");
+        }
+        // Note: for this project we don't have to check for the price of the token (star)
+        // Get addresses of the two token owners
+        address _ownerTokenId1 = ownerOf(_tokenId1);
+        address _ownerTokenId2 = ownerOf(_tokenId2);
 
-        return string(abi.encodePacked(_ra, _dec, _mag));
+        if (_ownerTokenId1 == _ownerTokenId2) {
+            revert("You already own both stars");
+        }
+
+        // Exchange the star tokens
+        _transferFrom(_ownerTokenId1, _ownerTokenId2, _tokenId1);
+        _transferFrom(_ownerTokenId2, _ownerTokenId1, _tokenId2);
     }
 
-    function isStarRegistered(string memory _ra, string memory _dec, string memory _mag) public view returns (bool) {
-        return starRegistry[this.append(_ra, _dec, _mag)];
+    /**
+       Transfer a sender's star to another address
+      */
+    function transferStar(address _to1, uint256 _tokenId) public {
+        // Verify that the sender owns the star token
+        if (msg.sender != ownerOf(_tokenId)) {
+            revert("You need to be the owner of one of the stars to exchange");
+        }
+        _transferFrom(msg.sender, _to1, _tokenId);
     }
+
+    // Function that allows you to convert an address into a payable address
+    function _makePayable(address x) internal pure returns (address payable) {
+        return address(uint160(x));
+    }
+
 }
